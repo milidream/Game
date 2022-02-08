0d2e2b5f1b32170746ced4a8b37664cf3410d1d8
milidream <bchekir07@gmail.com>
2022-02-08 19:35:15 +0100

    pdpdpdpdp hedy

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 let game = document.querySelector("#game");
 let button = document.querySelector("#play");
-let newDiv = document.createElement("div")
+// let game = document.createElement("div");
+// let firstDiv = document.createElement("div");
+// let secondDiv = document.createElement("div");
+// let thirdDiv = document.createElement("div");
+// let firstCircle = document.createElement("div");
+// let secondCircle = document.createElement("div");
+// let thirdCircle = document.createElement("div");
+// let fourthCircle = document.createElement("div");
+// let chrono = document.createElement("h2");
 
 function start(){
-    let firstDiv = document.createElement("div");
-    let secondDiv = document.createElement("div");
-    let thirdDiv = document.createElement("div");
-    let firstCircle = document.createElement("div");
-    let secondCircle = document.createElement("div");
-    let thirdCircle = document.createElement("div");
-    let fourthCircle = document.createElement("div");
-    let chrono = document.createElement("h2")
+    game.removeAttribute("id", "game")
+    game.setAttribute("id", "games")
     while(game.firstChild) {
         game.removeChild(game.firstChild);
     };
-    game.append(firstDiv, secondDiv, thirdDiv);
-    firstDiv.append(firstCircle, secondCircle);
-    secondDiv.appendChild(chrono);
-    thirdDiv.append(thirdCircle, fourthCircle);
-}
+    for(let i=1;i<6;i++){
+        let elementDiv = document.createElement("div");
+        if(i==3){
+            elementDiv.setAttribute("id", "chrono");
+            game.append(elementDiv);
+        }else{
+            elementDiv.setAttribute("id", "circle" + i);
+            elementDiv.setAttribute("class", "circle");
+            game.append(elementDiv);
+        };
+    };
+};
 button.addEventListener("click", start);
