59e5d1ef6df2b1e7d3889d4393bf7816ae30a655
milidream <bchekir07@gmail.com>
2022-02-13 17:19:36 +0100

    33

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-let game = document.querySelector("#game");
-let button = document.querySelector("#play");
-// let game = document.createElement("div");
-// let firstDiv = document.createElement("div");
-// let secondDiv = document.createElement("div");
-// let thirdDiv = document.createElement("div");
-// let firstCircle = document.createElement("div");
-// let secondCircle = document.createElement("div");
-// let thirdCircle = document.createElement("div");
-// let fourthCircle = document.createElement("div");
-// let chrono = document.createElement("h2");
+const game = document.querySelector("#game");
+const games = document.querySelector("#games");
+const button = document.querySelector("#play");
+let score = 0;
 
 function start(){
     game.removeAttribute("id", "game")
@@ -16,19 +9,41 @@ function start(){
     while(game.firstChild) {
         game.removeChild(game.firstChild);
     };
-    for(let i=1;i<6;i++){
-        let elementDiv = document.createElement("div");
-        if(i==3){
-            elementDiv.setAttribute("id", "chrono");
-            let chrono = document.createTextNode("0:00")
-            elementDiv.appendChild(chrono)
-            game.append(elementDiv);
-        }else{
+    for(let i=1;i<5;i++){
+        const elementDiv = document.createElement("div");
             elementDiv.setAttribute("id", "circle" + i);
             elementDiv.setAttribute("class", "circle");
             game.append(elementDiv);
-        };
     };
-    
+    setGreen();
+};button.addEventListener("click", start);
+
+function setGreen(){
+    let nb = Math.floor((Math.random() * 4) + 1);
+    let element = document.querySelector("#circle" + nb);
+    element.style.backgroundColor = "#3DCD6E";
+    element.dataset.correct = true;
 };
-button.addEventListener("click", start);
+
+function check(e){
+    const timeOut = setTimeout(gameOver, 3200);
+    const circle = document.querySelectorAll(".circle");
+    let selected = e.target;
+    let correct = selected.dataset.correct;
+    if(correct){
+        clearTimeout(timeOut);
+        circle.forEach(button=>button.removeAttribute('style'));
+        circle.forEach(button=>button.removeAttribute('data-correct'));
+        setGreen();
+        score++
+        console.log(score);
+    }else{
+        console.log("pd");
+    }
+};game.addEventListener("click", check);
+
+function gameOver() {
+    while(game.firstChild) {
+        game.removeChild(game.firstChild);
+    };
+};
